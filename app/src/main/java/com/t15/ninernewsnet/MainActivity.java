0843d05ccf99716282867fa458dc084d5a9e2a5f
Jakeem <rsmit281@uncc.edu>
2020-11-12 17:33:11 -0500

    login functionality commit

--- app/src/main/java/com/t15/ninernewsnet/MainActivity.java
+++ app/src/main/java/com/t15/ninernewsnet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.t15.ninernewsnet;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        String displayname;
+
+        Intent myIntent = getIntent();
+        displayname = myIntent.getStringExtra("name");
+        getSupportActionBar().setTitle(displayname);
 
 
         /*
