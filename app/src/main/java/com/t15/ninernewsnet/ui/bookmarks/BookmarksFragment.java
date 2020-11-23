6a0676576af2943fc88b720965f8c1124a07294f
NaseemJones <naseem7894@gmail.com>
2020-11-23 07:08:54 +0000

    Replace BookmarksFragment.java

--- app/src/main/java/com/t15/ninernewsnet/ui/bookmarks/BookmarksFragment.java
+++ app/src/main/java/com/t15/ninernewsnet/ui/bookmarks/BookmarksFragment.java
@@ -12,8 +12,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.t15.ninernewsnet.CustomAdapter;
 import com.t15.ninernewsnet.R;
+
+import java.util.ArrayList;
 
 public class BookmarksFragment extends Fragment {
 
@@ -25,6 +31,13 @@ public class BookmarksFragment extends Fragment {
                 ViewModelProviders.of(this).get(BookmarksViewModel.class);
         View root = inflater.inflate(R.layout.fragment_bookmarks, container, false);
         //final CardView cardView = root.findViewById(R.id.itemCard);
+        RecyclerView recyclerView = root.findViewById(R.id.recycleViewLayout2);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        ArrayList<String> textData = new ArrayList<>();
+        textData.add("Megan Bird, a UNC Charlotte senior from Charlottesville, Virginia, is among finalists in contention for the Rhodes Scholarship, one of the world’s most prestigious graduate fellowships.");
+        CustomAdapter myAdapter = new CustomAdapter(textData);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         final TextView textView = root.findViewById(R.id.text_bookmarks);
         bookmarksViewModel.getText().observe(this, new Observer<String>() {
